761b1b8d15ae56d8aca3781dbd17f89cd6cd2c8f
rhezapandya <rheza20001@mail.unpad.ac.id>
2022-11-30 13:06:03 +0700

    deploy

---
+++ project-dss-wp-beasiswa/resources/views/index.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Projek Decision Support System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/css/indexStyle.css') }}">
+    <link rel="icon" href="{{ asset('/image/unpad.png') }}" type="image">
+</head>
+
+<body>
+    <section class="h-100 gradient-form" style="background-color: #eee;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-xl-10">
+                    <div class="card rounded-3 text-black">
+                        <div class="row g-0">
+                            <div class="col-lg-6 mt-5 mb-5">
+                                <div class="card-body p-md-5 mx-md-4">
+
+                                    <div class="text-center">
+                                        <h4 class="mt-1 mb-2">Project Decision Support System</h4>
+                                        <h5 class="mt-1 mb-3">Pemberian Beasiswa S1 terhadap Pelajar SMA di Jatinangor</h5>
+                                        <ul style="text-align:justify; padding:0">
+                                            <li>
+                                                <p style="margin:0">Affan Rifqy Kurniadi<br>(140810200003)</p>
+                                            </li>
+                                            <li>
+                                                <p style="margin:0">Osmond Haylim<br>(140810200022)</p>
+                                            </li>
+                                            <li>
+                                                <p style="margin:0">Rheza Pandya Andhikaputra<br>(140810200023)</p>
+                                            </li>
+                                            <li>
+                                                <p style="margin:0">Andyka Baswara Putra<br>(140810200061)</p>
+                                            </li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="row align-items-center justify-content-center text-center mt-5">
+                                        <div class="col align-items-center justify-content-center">
+                                            <a href="">
+                                                <button class="btn btn-primary btn-index text-index">
+                                                    Start Program
+                                                </button>
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                    <h4 class="mb-4">We are more than just a company</h4>
+                                    <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
